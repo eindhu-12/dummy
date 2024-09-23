2e0c4e017fb65df3e88c183e34387de2e31de52a
unknown <rapoluindhu2929@gmail.com>
2024-09-23 16:56:26 +0530

    dummy files

---
+++ Task-2 Slice/slice.js
@@ -0,0 +1,7 @@
+var s1="Hello My Name Is Eindhu"
+console.log("String:---",s1);
+var s2=s1.slice(5,13)
+console.log("extracting Middle part of string:--",s2);
+console.log("extracted the last char of portion:--",s2.slice(-1));
+console.log("Coverting part of string LowerCase:--",s2.toLocaleLowerCase());
+
---
+++ task-3 char adding/char.js
@@ -0,0 +1,10 @@
+var a="A"
+var b="carrot"
+console.log("char:--",a);
+console.log("String:--",b);
+var c =a.concat(" ",b)
+console.log("concating the char and string:--",c);
+console.log("converting string to UpperCase:--",c.toUpperCase());
+console.log("Last second char of String:--",c[c.length-2]);
+
+
